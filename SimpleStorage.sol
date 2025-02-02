30da716feb55fe5d57f08c88e421cc4e28938210
jbucsa <justin.bucsa@gmail.com>
2025-02-01 23:21:51 -0600

    Create unit type reference for contracts.

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,5 +3,23 @@
 pragma solidity >=0.8.19 <0.9.0; // Our versions and range of versions that will be acceptable.
 
 contract SimpleStorage{
-    
+
+    uint256 favoriteNumber;
+
+    function store( uint256 _favoriteNumber) public{
+        favoriteNumber = _favoriteNumber;
+    }
+
 }
+
+contract UnitsReference{
+    //  Baasic Types: booleans, uint, int, address, bytes
+    bool hasFavoriteNumber = true;
+    uint256 wantFavoriteNumber2 = 88; 
+    // If not value is given, default value will be zero.
+    int256 needsANumber = -99;
+    string WantMeToTalk = "Say Hello";
+    // address myAddress = 00xaB1B72063d7dd7cD1aa65511aA6ABD224B0B560;
+    bytes32 myFavoriteByte32 = "dog";
+
+}  
