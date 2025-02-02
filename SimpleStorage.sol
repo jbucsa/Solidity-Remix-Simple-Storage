76a716af9907fa8882e3a0c65de2e65f7d71890d
jbucsa <justin.bucsa@gmail.com>
2025-02-01 19:22:14 -0600

    First basic Solidity contract created.

---
+++ SimpleStorage.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+// ^License version is required to compile the code.
+pragma solidity >=0.8.19 <0.9.0; // Our versions and range of versions that will be acceptable.
+
+contract SimpleStorage{
+    
+}
